f987b389f568fbfec15918cc042a9fc65ad99ef5
Gorkeur <Alexandre.bar92fr@gmail.com>
2023-10-06 17:37:39 +0200

    Ajoutez des fichiers projet.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+            {
+                CounterBtn.Text = $"Clicked {count} time";
+                toto.TextColor = Colors.Green;
+            }
+            else
+            {
+                CounterBtn.Text = $"Clicked {count} times";
+                toto.Text = count.ToString();
+                toto.TextColor = Colors.Red;
+                SemanticScreenReader.Announce(CounterBtn.Text);
+            }
+        }
+    }
+}
---
+++ MauiApp1/Modeles/Etudiant.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Modeles
+{
+    internal class Etudiant
+    {
+        #region Attributs
+        public static List<Etudiant> Collclasse = new List<Etudiant>();
+
+        private string _nom;
+        private string _prenom;
+        private string _image;
+
+        #endregion
+
+        #region Constructeur
+        public Etudiant(string nom, string prenom, string image)
+        {
+            _nom = nom;
+            _prenom = prenom;
+            _image = image;
+
+            Etudiant.Collclasse.Add(this);
+        }
+        #endregion
+
+        #region Getters/Setters
+
+        public string Getnom()
+        {
+            return _nom;
+        }
+        public void Setnom(string nom)
+        {
+            _nom = nom;
+        }
+        public string Getprenom()
+        {
+            return _prenom;
+        }
+        public void Setprenom(string prenom)
+        {
+            _prenom = prenom;
+        }
+        public string Getimage()
+        {
+            return _image;
+        }
+        public void Setimage(string image)
+        {
+            _image = image;
+        }
+
+        #endregion
+
+        #region Methodes
+        #endregion
+    }
+}
